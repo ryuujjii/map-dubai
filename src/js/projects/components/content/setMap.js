c60dcc221c00f268292ab2eefb063d8355321f4a
Javlon Talipov <javlon.talipov13@mail.ru>
2024-08-21 12:24:23 +0500

    refactor center map
    перенес центрирование карты в изменении картинки карты

--- src/js/projects/components/content/setMap.js
+++ src/js/projects/components/content/setMap.js
@@ -3,12 +3,30 @@ function getSetMapFn() {
   const map = document.querySelector('[data-project="map"]');
   const mapShadow = document.querySelector('[data-project="map-shadow"]');
   imgLoader(map, mapShadow);
-  return function ({ shadow, img }) {
+  return function ({ shadow, img, centerPosotion }) {
     map.setAttribute('src', img);
     mapShadow.setAttribute('src', shadow);
+    centerImg(centerPosotion);
   };
 }
 
 const setMap = getSetMapFn();
 
 export default setMap;
+
+function centerImg(centerPosotion) {
+  const containerWidth = document.querySelector('.masterplan__media').offsetWidth;
+  let res = Math.abs(containerWidth / 2 - window.innerWidth / 2 - containerWidth * centerPosotion);
+  gsap.to('.masterplan__wrapper', {
+    scrollTo: {
+      x: 0
+    },
+    onComplete: () => {
+      gsap.to('.masterplan__wrapper', {
+        scrollTo: {
+          x: res
+        }
+      });
+    }
+  });
+}
