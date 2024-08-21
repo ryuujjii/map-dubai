7d250d8e79d1b689ab34e3341adef0f6c63b3ae9
Quandiq Kalbaev <quandiqkalbaev@gmail.com>
2024-08-21 16:09:34 +0500

    fix 992 popups adaptive

--- src/js/projects/components/replaceBlocks.js
+++ src/js/projects/components/replaceBlocks.js
@@ -4,12 +4,7 @@ export default function replaceBlocks() {
   let developeIMGBlock = document.querySelector(".developer__media");
   let developerInfoBlock = document.querySelector(".developer__info");
 
-  let broCont = document.querySelector(".brochure__container");
-  let broText = document.querySelector(".brochure__text-p");
-
   if (queryMatches(768)) {
     developerInfoBlock.appendChild(developeIMGBlock);
-    broCont.appendChild(broText);
   }
-
 }
