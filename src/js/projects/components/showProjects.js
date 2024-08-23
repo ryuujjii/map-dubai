80e86bc389257d98404af9d2d23cf44f7e5d2ec4
Quandiq Kalbaev <quandiqkalbaev@MacBook-Air-Quandiq.local>
2024-08-23 05:45:30 -0700

    fix active btn project

--- src/js/projects/components/showProjects.js
+++ src/js/projects/components/showProjects.js
@@ -2,15 +2,15 @@ export default function showProjects() {
   let showProjects = document.querySelector(".project__popupBtns-open");
   let projectsWrapper = document.querySelector(".project__popupBtns-wrapper");
   showProjects.addEventListener("click", () => {
-    projectsWrapper.classList.toggle("active");
+    projectsWrapper.classList.toggle("activee");
   });
   window.addEventListener("wheel", (e) => {
-    projectsWrapper.classList.remove("active");
+    projectsWrapper.classList.remove("activee");
   });
   document.addEventListener("click", (e) => {
     if (e.target.closest(".project__popupBtns-wrapper")) {
       return;
     }
-    projectsWrapper.classList.remove("active");
+    projectsWrapper.classList.remove("activee");
   });
 }
