20aa8141e4882e6124b83c5ed600bc50f7d099ea
Javlon Talipov <javlon.talipov13@mail.ru>
2024-08-26 15:16:12 +0500

    add loader for pano

--- src/js/sections/pano.js
+++ src/js/sections/pano.js
@@ -6,18 +6,32 @@ import testBtns from "@/components/testBtns";
 export default function pano() {
   const btns = document.querySelectorAll('[data-modal-open]');
   const modalIframe = document.querySelector(".pano__iframe");
-
+  let isPanoLoaded = false;
   btns.forEach(btn => {
     btn.addEventListener("click", (e) => {
-      insertIframe(modalIframe, btn.getAttribute('data-modal-open'));
-      addClassName(document.body, 'open-pano');
-      collectEscEls('close-pano');
+      removeClassName(document.documentElement, 'preloader-hidden');
+      setTimeout(() => {
+        insertIframe(modalIframe, btn.getAttribute('data-modal-open'));
+        addClassName(document.body, 'open-pano');
+        collectEscEls('close-pano');
+      }, 1000);
     });
   });
-  !__SHOWPANO__ ? testBtns() : null;
+  if (__SHOWPANO__) {
+    window.addEventListener("pano-loaded", (e) => {
+      if (isPanoLoaded) return;
+      isPanoLoaded = true;
+      setTimeout(() => {
+        addClassName(document.documentElement, 'preloader-hidden');
+      }, 1000);
+    });
+  } else {
+    testBtns();
+  }
   window.addEventListener('close-pano', (e) => {
     removeClassName(document.body, 'open-pano');
     removeLastEscEl('close-pano');
+    isPanoLoaded = false;
   });
 };
 
