ff41b888e314f4a57b2fb63247b4ad887dab36ab
Javlon Talipov <javlon.talipov13@mail.ru>
2024-08-28 19:36:57 +0500

    add handler for classNames in brochure project

--- src/js/projects/components/content/popups/brochure.js
+++ src/js/projects/components/content/popups/brochure.js
@@ -1,14 +1,18 @@
 import { setAttr } from 'utils';
 import imgLoader from '@/projects/components/content/imgLoader';
-import { getProjectItem } from '@/projects/components/content/utils';
+import { getProjectItem, getHandleClassNames } from '@/projects/components/content/utils';
 
 function getBrochurePopupFn() {
   const brochureEls = {
     brochureImg: getProjectItem('brochureImg'),
+    brochure: getProjectItem('brochure'),
   };
+  const handleClassNames = getHandleClassNames(brochureEls.brochure);
+
   imgLoader(brochureEls.brochureImg);
-  return function brochurePopup({ brochureImg }) {
+  return function brochurePopup({ brochureImg, classNames }) {
     setAttr(brochureEls.brochureImg, "src", brochureImg);
+    handleClassNames(classNames)
   };
 }
 
