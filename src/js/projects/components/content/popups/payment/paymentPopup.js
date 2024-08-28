530eb3dd5c14c32d9666666b7f9c9be52a51bd77
Javlon Talipov <javlon.talipov13@mail.ru>
2024-08-28 20:05:37 +0500

    add handler for classNames in payment

--- src/js/projects/components/content/popups/payment/paymentPopup.js
+++ src/js/projects/components/content/popups/payment/paymentPopup.js
@@ -1,14 +1,20 @@
 import getPaymentItems from '@/projects/components/content/popups/payment/getPaymentItems';
 import desctriptionLayout from '@/projects/components/content/popups/payment/desctriptionLayout';
-import { getPaintedItems, getProjectItem } from '@/projects/components/content/utils';
+import { getPaintedItems, getProjectItem, getHandleClassNames } from '@/projects/components/content/utils';
+
 function getPaymentPopupFn() {
   const paymentEls = {
     paymentItems: getProjectItem('paymentItems'),
     paymentDescriptions: getProjectItem('paymentDescriptions'),
   };
+  const itemshandleClassNames = getHandleClassNames(paymentEls.paymentItems);
+  const descriptionshandleClassNames = getHandleClassNames(paymentEls.paymentDescriptions);
+
   return function paymentPopup({ items, descriptions }) {
-    getPaymentItems(paymentEls.paymentItems, items);
-    getPaintedItems(paymentEls.paymentDescriptions, descriptions, desctriptionLayout);
+    getPaymentItems(paymentEls.paymentItems, items.content);
+    getPaintedItems(paymentEls.paymentDescriptions, descriptions.content, desctriptionLayout);
+    itemshandleClassNames(items.classNames);
+    descriptionshandleClassNames(descriptions.classNames);
   };
 };
 const paymentPopup = getPaymentPopupFn();
