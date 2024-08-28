dbb4d52b3afb7032591c40c5876b91474c438603
Javlon Talipov <javlon.talipov13@mail.ru>
2024-08-28 16:12:26 +0500

    add center abaout location map

--- src/js/projects/components/content/popups/about/locationDot.js
+++ src/js/projects/components/content/popups/about/locationDot.js
@@ -1,8 +1,4 @@
-export default function locationDot(projectLocationDot, content) {
-  projectLocationDot.innerHTML = getLocationDotLayout(content);
-};
-
-
+import { removeClassName, addClassName } from 'utils';
 
 function getLocationDotLayout({ dot }) {
   return `
@@ -19,3 +15,43 @@ function getLocationDotLayout({ dot }) {
     </foreignObject>
   `;
 }
+function getCenterLocationFn() {
+  const locationScroll = document.querySelector('.location-about__map-scroll');
+  // const locationMedia = document.querySelector('.location-about__map-container');
+  return function centerLocation({ dot }) {
+    removeClassName(locationScroll, 'no-scroll');
+
+    // let x = calculateNewPos(locationScroll.offsetWidth, locationMedia.offsetWidth, dot.centerPosotion.x);
+    // let y = calculateNewPos(locationScroll.offsetHeight, locationMedia.offsetHeight, dot.centerPosotion.y);
+    console.log(locationScroll.offsetHeight / 2);
+    gsap.to(locationScroll, {
+      scrollTo: {
+        x: 0,
+        y: 0
+      },
+      onComplete: () => {
+        gsap.to(locationScroll, {
+          scrollTo: {
+            x: '.location-dot',
+            y: '.location-dot',
+            offsetX: locationScroll.offsetWidth / 2,
+            offsetY: locationScroll.offsetHeight / 2
+          },
+          onComplete() {
+            addClassName(locationScroll, 'no-scroll');
+          }
+        });
+      }
+    });
+  };
+  function calculateNewPos(parentSize, mediaSize, offset) {
+    return Math.abs(mediaSize / 2 - parentSize / 2 - mediaSize * offset);
+  }
+}
+
+const centerLocation = getCenterLocationFn();
+
+export default function locationDot(projectLocationDot, content) {
+  projectLocationDot.innerHTML = getLocationDotLayout(content);
+  centerLocation(content);
+};
