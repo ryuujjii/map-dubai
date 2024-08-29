523b58c78a59ed8dae16a4f3546853d525988fa3
Arina <terekhinaarina00@gmail.com>
2024-08-29 17:30:47 +0500

    добавить нвоый проект keturah

--- src/js/components/testBtns.js
+++ src/js/components/testBtns.js
@@ -4,7 +4,8 @@ export default function testBtns() {
   const btns = document.querySelectorAll('[data-test]');
   btns.forEach(btn => {
     btn.addEventListener("click", (e) => {
-      dispatchCustomEvent({ el: window, event: "show-project", detail: { projectId: btn.getAttribute('data-test') } });
+      // btn.getAttribute('data-test')
+      dispatchCustomEvent({ el: window, event: "show-project", detail: { projectId: "keturah-resort" } });
     });
   });
 };
