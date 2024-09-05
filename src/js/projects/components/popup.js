8ba599f53e45a34ddf6317afbf110ba5db387793
Arina <terekhinaarina00@gmail.com>
2024-09-05 12:14:38 +0500

    добавить рендеры gallery beachfront и добавить проект urban life

--- src/js/projects/components/popup.js
+++ src/js/projects/components/popup.js
@@ -1,7 +1,11 @@
 import ViewPopup from "@/projects/components/popup/viewPopup";
 import projectPopup from "@/projects/components/projectPopup";
+import tabPayment from "@/projects/components/popup/tabPayment";
+
+
 
 export default function popup(params) {
   new ViewPopup();
   projectPopup();
+  tabPayment()
 };
