572d9fc377554a6fabeadf6081fb5f7663f02915
iuxeps <islom.kdev@gmail.com>
2024-09-09 11:55:41 +0500

    optimize popup and adaptive

--- src/js/components/popup.js
+++ src/js/components/popup.js
@@ -1,34 +1,44 @@
 export function popup() {
-    const popupOpenBtns = document.querySelectorAll('[data-marker-popup-btn]')
-    const popups = document.querySelectorAll('.home-popup')
+    const popupOpenBtns = document.querySelectorAll('[data-marker-popup-btn]');
+    const popups = document.querySelectorAll('.home-popup');
 
     popupOpenBtns.forEach(btn => {
         btn.addEventListener('click', () => {
-            const currentBtn = btn.getAttribute('data-marker-popup-btn')
+            const currentBtn = btn.getAttribute('data-marker-popup-btn');
 
             popups.forEach(popup => {
-                const currentPopup = popup.getAttribute('data-marker-popup')
-
-                if (currentBtn === currentPopup) {
-                    setTimeout(() => {
-                        popup.classList.add('_show')
-                        document.body.classList.add('hide-light-map')
-                    }, 500);
+                if (popup.getAttribute('data-marker-popup') === currentBtn) {
+                    popup.classList.add('_show');
+                    document.body.classList.add('hide-light-map');
                 }
 
                 popup.addEventListener('click', (e) => {
-                    const popupClick = e.target
-
-                    if (popupClick.classList.contains('home-popup__close') || popupClick.classList.contains('home-popup')) {
-                        popup.classList.remove('_show')
-                        document.body.classList.remove('hide-light-map')
+                    if (e.target.classList.contains('home-popup__close') || e.target.classList.contains('home-popup')) {
+                        popup.classList.remove('_show');
+                        document.body.classList.remove('hide-light-map');
                     }
+                });
+            });
+        });
+    });
 
-                })
+    const allInputPhone = document.querySelectorAll(`input[type="tel"]`)
 
+    allInputPhone.forEach(phone => {
+        phone.addEventListener('input', function (e) {
+            let value = e.target.value;
 
-            })
-        })
+            value = value.replace(/[^0-9+]/g, '');
+
+            if (value.indexOf('+') > 0) {
+                value = value.replace(/\+/g, '');
+            } else if (value.indexOf('+') === 0) {
+                value = '+' + value.slice(1).replace(/\+/g, '');
+            }
+
+            e.target.value = value;
+        });
+
     })
 
 }
