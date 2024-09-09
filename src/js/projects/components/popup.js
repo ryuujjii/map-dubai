d0ad65bf4b65b77caed05520d5b471cd939d7f9b
alladdin.work <alladdin.work@gmail.com>
2024-09-09 17:21:26 +0500

    remove console log popup.js

--- src/js/projects/components/popup.js
+++ src/js/projects/components/popup.js
@@ -8,8 +8,6 @@ export default function popup(params) {
   const scrollmouse = document.querySelectorAll('.popup__scrollmouse');
   popupWp.forEach((pop, i) => {
     pop.addEventListener('scroll', (e) => {
-      console.log(e.target.scrollTop);
-
       if (e.target.scrollTop > 100) {
         scrollmouse[i].classList.add('removed');
       } else {
