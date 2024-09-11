64d21fcb3bfc8d0699812bb664e27102cf46e050
Javlon Talipov <javlon.talipov13@mail.ru>
2024-09-11 17:09:55 +0500

    add cache disable for viewer

--- src/js/modal360/components/viewer/viewerInstance.js
+++ src/js/modal360/components/viewer/viewerInstance.js
@@ -1,11 +1,12 @@
 import { queryMatches } from 'utils';
-import { Viewer } from '@photo-sphere-viewer/core';
+import { Viewer, Cache } from '@photo-sphere-viewer/core';
 import { GyroscopePlugin } from '@photo-sphere-viewer/gyroscope-plugin';
 /**
    * viewer initialization
    */
 const IS_IPADE = queryMatches(1170);
 
+Cache.enabled = false;
 const viewerInstance = new Viewer({
   plugins: [GyroscopePlugin],
   container: document.querySelector(".viewer__wrapper"),
