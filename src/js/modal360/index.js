128b356f6773ed4a0dc917b852fbc5557663e013
Arina <terekhinaarina00@gmail.com>
2024-09-12 13:04:04 +0500

    добавить стили для modal360

--- src/js/modal360/index.js
+++ src/js/modal360/index.js
@@ -1,18 +1,21 @@
-import '../../scss/modal360/style.scss';
+import "../../scss/modal360/style.scss";
 import { gallery } from "@/modal360/components/gallery";
 import { viewer } from "@/modal360/components/viewer/viewer.js";
 import { removeClassToAlter } from "@/modal360/components/alterParentWindow.js";
 import updateInstanceOnContentChange from "@/modal360/components/updateInstanceOnContentChange.js";
+import { isMobileOrTablet, addClassName } from "@/components/utils";
+import recieveMsg from "@/modal360/content/recieveMsg";
 
-import recieveMsg from '@/modal360/content/recieveMsg';
-
-window.addEventListener('DOMContentLoaded', (params) => {
+window.addEventListener("DOMContentLoaded", (params) => {
+  isMobileOrTablet()
+    ? addClassName(document.documentElement, "mob-device")
+    : null;
   recieveMsg();
   viewer();
   gallery();
   updateInstanceOnContentChange();
 });
 
-window.addEventListener('unload', function (event) {
+window.addEventListener("unload", function (event) {
   removeClassToAlter();
 });
