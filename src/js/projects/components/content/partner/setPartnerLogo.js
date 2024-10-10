e830b749ab0a79fd3eda3aa386d16b41f91db78d
Javlon Talipov <javlon.talipov13@mail.ru>
2024-10-10 17:26:24 +0500

    add partner info to jsons

--- src/js/projects/components/content/partner/setPartnerLogo.js
+++ src/js/projects/components/content/partner/setPartnerLogo.js
@@ -1,16 +1,19 @@
 import imgLoader from '@/projects/components/content/imgLoader';
-import { getProjectItem, getHandleClassNames } from '@/projects/components/content/utils';
+import { getProjectItem, getHandleClassNames, getPaintedItems } from '@/projects/components/content/utils';
+import paymentPlanLayout from '@/projects/components/content/partner/paymentPlanLayout';
 
 function getSetPartnerLogoFn() {
   const partnerEls = {
     partnerLogoParent: getProjectItem("partnerLogoParent"),
     partnerLogo: getProjectItem("logo"),
+    partnerInfo: getProjectItem("partnerInfo"),
   };
   // const handleClassNames = getHandleClassNames(partnerEls.partnerLogoParent);
   imgLoader(partnerEls.partnerLogo);
-  return function setPartnerLogo({ logo, classNames }) {
+  return function setPartnerLogo({ logo, classNames, info }) {
     // handleClassNames(classNames);
     partnerEls.partnerLogo.setAttribute('src', logo);
+    getPaintedItems(partnerEls.partnerInfo, info, paymentPlanLayout);
   };
 };
 
